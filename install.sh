a6edc431b9dcc22079c18068816ce441c29eb3ab
jsirianni <jsirianni@ws1.sir.local>
2017-09-10 18:51:16 -0400

    started install file

--- install.sh
+++ install.sh
@@ -12,11 +12,18 @@
 user=gocron
 password=$1
 
+# Create user
+sudo adduser \
+      --system \
+      --shell /bin/false \
+      --disabled-password \
+      $user
+
 # Install postgres
-apt-get update
-apt-get install postgresql-9.6 -y
-systemctl enable postgresql
-systemctl start postgresql
+sudo apt-get update
+sudo apt-get install postgresql-9.6 wget nano -y
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
 
 # Configure postgres
 sudo -u postgres psql -c "CREATE DATABASE gocron;"
@@ -25,3 +32,26 @@ sudo -u postgres psql -c "CREATE USER $user WITH PASSWORD '$password';"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON gocron TO $user;"
 
 # Get gocron binary
+sudo mkdir /usr/local/bin
+wget -O /usr/local/bin/gocron https://github.com/jsirianni/gocron/blob/master/bin/cronserver?raw=true
+
+# Get gocron config and configure it
+sudo mkdir -p ~/.config/gocron
+wget -O ~/.config/gocron/.config.yml https://raw.githubusercontent.com/jsirianni/gocron/master/src/.example.config.yml
+sudo chmod 600 ~/.config/gocron/.config.yml
+sudo nano ~/.config/gocron/.config.yml
+
+# Build systemd service
+sudo touch /etc/systemd/system/gocron.service
+
+echo "[Unit]" > /etc/systemd/system/gocron.service
+echo "Description=GOCron Monitoring Service" >> /etc/systemd/system/gocron.service
+echo "After=network.target" >> /etc/systemd/system/gocron.service
+
+echo "[Service]" >> /etc/systemd/system/gocron.service
+echo "User=$user" >> /etc/systemd/system/gocron.service
+echo "ExecStart=/home/nanodano/my_daemon --option=123" >> /etc/systemd/system/gocron.service
+echo "Restart=on-abort" >> /etc/systemd/system/gocron.service
+
+echo "[Install]" >> /etc/systemd/system/gocron.service
+echo "WantedBy=multi-user.target" >> /etc/systemd/system/gocron.service
