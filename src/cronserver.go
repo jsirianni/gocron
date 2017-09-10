8f3667c55fad8ac476fac6ca418d4e2f187bd749
jsirianni <jsirianni@ws1.sir.local>
2017-09-09 21:31:39 -0400

    database fully working

--- src/cronserver.go
+++ src/cronserver.go
@@ -11,7 +11,7 @@ import (
       "database/sql"; _ "github.com/lib/pq";
 )
 
-// Structs
+
 type Config struct {
       Dbfqdn string
       Dbport string
@@ -34,37 +34,33 @@ type Cron struct {
       lastRunTime string  // Unix timestamp
 }
 
-
-// Global Vars
 var config Config
 
 
 func main() {
-      // Read config file from user's home directory
       user, err := user.Current()
+
       yamlFile, err := ioutil.ReadFile(user.HomeDir + "/.config/gocron/.config.yml")
       if err != nil {
             panic(err)
       }
+
       err = yaml.Unmarshal(yamlFile, &config)
       if err != nil {
             panic(err)
       }
 
-      // Start HTTP server
       http.HandleFunc("/", cronStatus)
       http.ListenAndServe(":8080", nil)
 }
 
 
 
-// Parse GET Request parameters
-// TODO Add validation
 func cronStatus(w http.ResponseWriter, r *http.Request) {
-      // Build variables and assign values to cronJob
-      var cronJob Cron
       var currentTime = int(time.Now().Unix())
       var socket = strings.Split(r.RemoteAddr, ":")
+
+      var cronJob Cron
       cronJob.cronName = r.URL.Query().Get("cronName")
       cronJob.account = r.URL.Query().Get("account")
       cronJob.email = r.URL.Query().Get("email")
@@ -73,35 +69,45 @@ func cronStatus(w http.ResponseWriter, r *http.Request) {
       cronJob.tolerance = r.URL.Query().Get("tolerance")
       cronJob.lastRunTime = strconv.Itoa(currentTime)
 
-      updateDatabase(cronJob)
+
+      go updateDatabase(cronJob)
 }
 
 
-// Insert or update a cron entry in the database
 func updateDatabase(c Cron) {
-      // Build postgres connection string
-      var connectionString = "postgres://" +
+      var connectionString string
+      var query string
+
+      connectionString = "postgres://" +
       config.Dbuser + ":" +
       config.Dbpass + "@" +
       config.Dbfqdn +
       "/gocron" +
       "?sslmode=disable"
 
+      query = "INSERT INTO gocron (cronname, account, email, ipaddress, crontime, tolerance, lastruntime) VALUES ('" +
+             c.cronName + "','" +
+             c.account + "','" +
+             c.email + "','" +
+             c.ipAddress + "','" +
+             c.cronTime + "','" +
+             c.tolerance + "','" +
+             c.lastRunTime + "') " +
+             "ON CONFLICT (cronname, account) DO UPDATE " +
+             "SET lastruntime = " + "'" + c.lastRunTime + "'" +
+             ";"
 
-      // Check the database for the existing cron (primary key)
-      // If the cron exists, update its lastRun column with the current time
+      go log("Cron update from " + c.account + " at " + c.ipAddress + "\n" +
+      "Job: " + c.cronName + "\n" +
+      "Time: " + c.lastRunTime + "\n" + query + "\n")
+
       db, err := sql.Open("postgres", connectionString)
+      defer db.Close()
       if err != nil {
+            checkError(err)
             panic(err)
       }
-
-      _, err = db.Query("SELECT cronName FROM gocron WHERE account = " + c.account)
-
-
-      // If the cron does not exist, create a table entry and record lastRun with the current time
-      // Send an email alert notifying the user that the entry has been made
-
-      db.Close()
+      _, err = db.Exec(query)
 }
 
 // Check for missed cron updateDatabase
@@ -134,6 +140,11 @@ func alert(recipient string, subject string, message string) {
 // Handle errors that do not require the program to stop
 func checkError(err error) {
   if err != nil {
-    fmt.Printf("ERROR: " + err.Error() + "\n\n")
+    log(err.Error())
   }
+}
+
+// log the output
+func log(message string) {
+      fmt.Println(message)
 }
