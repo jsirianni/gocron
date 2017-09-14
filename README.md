a410eda630a5986ad2205ed89d43aa0516ddab3b
jsirianni <jsirianni@ws1.sir.local>
2017-09-13 22:26:16 -0400

    simplify install script and update readme

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ not run after a predetermined amount of time.
 
 Email alerts are sent one time and then suppressed. Alerts are re-triggered only if the job checks in again, and then misses its next run.
 
-### Usage
+## Usage
 Send a GET request with the following parameters in the query string
 - cronname
 - account
@@ -23,5 +23,28 @@ Append to an existing crontab entry with:
 
 The above examples will notify the server every hour with a tolerance of 2 minutes. If the job does not check in within 2 hours and 2 minutes, an email alert is sent. Future notifications are suppressed until the job checks in again.
 
-### Notes
+## Installing
+
+### Postgresql must be installed and listening on localhost
+`"CREATE DATABASE gocron;"`
+`"CREATE TABLE gocron(cronName varchar, account varchar, email varchar, ipaddress varchar, frequency varchar, tolerance int, lastruntime varchar, alerted boolean, PRIMARY KEY(cronname, account));"`
+`"CREATE USER gocron WITH PASSWORD 'password';"`
+`"GRANT ALL PRIVILEGES ON gocron TO gocron;"`
+
+### Run install.sh
+`chmod +x install.sh`
+`sudo ./install.sh`
+
+This script will:
+- Copy the gocron binary to `/usr/local/bin`
+- Copy the config example to `/etc/gocron`
+- Create a Systemd service
+
+## Notes
 The main purpose of this project is to gain familiarity with golang. If you have improvements or suggestions, please feel free to file an issue or open a pull request.
+
+## TODO
+
+Make compatible with Docker
+
+Build a Web front end for easy manegment
--- install.sh
+++ install.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
-
-# This script will install and configure go cron
-# with postgresql. Run as the user that will be
-# running the service as the config file will live in
-# ~/.config/gocron.
-
-# This script has been dested on Debian 9 with postgres 9.6
-# but may work with Debian 8 and Ubuntu 16.04.
-
-
-# Adjustable values
-password=$1
-
-
-# Install postgres
-sudo apt-get update
-sudo apt-get install postgresql-9.6 nano -y
-sudo systemctl enable postgresql
-sudo systemctl start postgresql
-
-
-# Configure postgres
-echo "CREATE DATABASE gocron;"
-sudo -u postgres psql -c "CREATE DATABASE gocron;"
-sudo -u postgres psql gocron -c "CREATE TABLE gocron(cronName varchar, account varchar, email varchar, ipaddress varchar, frequency varchar, tolerance int, lastruntime varchar, alerted boolean, PRIMARY KEY(cronname, account));"
-echo "CREATE TABLE gocron(cronName varchar, account varchar, email varchar, ipaddress varchar, frequency varchar, tolerance int, lastruntime varchar, alerted boolean, PRIMARY KEY(cronname, account));"
-sudo -u postgres psql gocron -c "CREATE USER gocron WITH PASSWORD '$password';"
-echo "CREATE USER gocron WITH PASSWORD '$password';"
-sudo -u postgres psql gocron -c "GRANT ALL PRIVILEGES ON gocron TO gocron;"
-echo "GRANT ALL PRIVILEGES ON gocron TO gocron;"
-sleep 10
-
-# Pull newest Build
-sudo git pull
+cd $(dirname $0)
 
 # Get gocron binary
 sudo mkdir /usr/local/bin
@@ -44,24 +11,18 @@ sudo chmod +x /usr/local/bin/gocron
 sudo mkdir -p /etc/gocron/
 sudo cp ./src/.example.config.yml /etc/gocron/.config.yml
 sudo chmod 600 /etc/gocron/.config.yml
-sudo nano /etc/gocron/.config.yml
-sleep 5
+sudo vi /etc/gocron/.config.yml
 
 # Build systemd service
 sudo touch /etc/systemd/system/gocron.service
-
 echo "[Unit]" > /etc/systemd/system/gocron.service
 echo "Description=GOCron Monitoring Service" >> /etc/systemd/system/gocron.service
 echo "After=network.target" >> /etc/systemd/system/gocron.service
-
 echo "[Service]" >> /etc/systemd/system/gocron.service
 echo "ExecStart=/usr/local/bin/gocron" >> /etc/systemd/system/gocron.service
-
 echo "[Install]" >> /etc/systemd/system/gocron.service
 echo "WantedBy=multi-user.target" >> /etc/systemd/system/gocron.service
-sleep 5
 
 # Enable the gocron service
-sudo systemctl enable gocron.service
-sudo systemctl start gocron.service
+sudo systemctl enable gocron.service && sudo systemctl start gocron.service
 sudo systemctl status gocron.service
