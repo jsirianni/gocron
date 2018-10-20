266c96008fb4566d1a6d51e1b701b0f544f7a90c
joe sirianni <joe.sirianni@bluemedora.com>
2018-10-20 15:06:33 -0400

    update build script

--- build_linux.sh
+++ build_linux.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-cd $(dirname $0)
 
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gocron
 
-rm ./bin/gocro*
-
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gocron-front -v ./src/frontend/
-mv gocron-front  ./bin/
-
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gocron-back -v ./src/backend/
-mv gocron-back  ./bin/
