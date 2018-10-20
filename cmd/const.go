701b42a2a8c6f8c1bba3cfea8a97215d2ac9196b
joe sirianni <joe.sirianni@bluemedora.com>
2018-10-20 14:54:15 -0400

    bump version

--- cmd/const.go
+++ cmd/const.go
@@ -1,7 +1,7 @@
 package cmd
 
 
-const VERSION string = "4.0.0"
+const VERSION string = "4.0.1"
 
 const missedJobs = "SELECT * FROM gocron WHERE (extract(epoch from now()) - lastruntime) > frequency;"
 const revivedJobs = "SELECT * FROM gocron WHERE alerted = true AND (extract(epoch from now()) - lastruntime) < frequency;"
