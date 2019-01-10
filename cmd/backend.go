89f2e1c2f13ae38a4c405d8597326a533eb8c3b3
jsirianni <joe.sirianni@bluemedora.com>
2019-01-09 20:10:08 -0500

    cleanup

--- cmd/backend.go
+++ cmd/backend.go
@@ -12,7 +12,6 @@ import (
 var backendCmd = &cobra.Command{
 	Use:   "backend",
 	Short: "Start the gocron backend server",
-	Long: "Start the gocron backend server, which alerts on missed jobs",
 	Run: func(cmd *cobra.Command, args []string) {
 		startBackend()
 	},
@@ -26,20 +25,10 @@ func init() {
 
 
 func startBackend() {
-	// Initilize the config struct
-	//config = GetConfig(verbose)
-
 	if summary == true {
-		// If verbose == true, summary will send to syslog AND the configured
-		// alert system
 		libgocron.GetSummary(verbose)
 		return
 	}
 
-	// create the gocron table, if not exists
-	if libgocron.CreateGocronTable(verbose) == false {
-		os.Exit(1)
-	}
-
-	libgocron.Timer(verbose)
+	libgocron.StartBackend(verbose)
 }
