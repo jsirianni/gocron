5032e63f02d2e14ecb1e5312c0f6f4ad59d6173c
jsirianni <joe.sirianni@bluemedora.com>
2019-03-31 18:54:29 -0400

    tests for database.go

--- libgocron/database_test.go
+++ libgocron/database_test.go
@@ -1,1 +1,32 @@
 package libgocron
+
+import (
+    "testing"
+)
+
+func TestqueryDatabase(t testing.T) {
+    g := getTestConfig()
+    q := "SELECR * FROM gocron;"
+
+    _, err := queryDatabase(g, q)
+    if err == nil {
+        t.Errorf("Expected queryDatabase() to return an error, due to no database to connect to")
+    }
+}
+
+func TestupdateDatabase(t testing.T) {
+    g := getTestConfig()
+    c := getTestCron()
+
+    if g.updateDatabase(c) == true {
+        t.Errorf("Expected updateDatabase() to return false, due to no database to connect to")
+    }
+}
+
+func TestcreateGocronTable(t testing.T) {
+    g := getTestConfig()
+
+    if err := g.createGocronTable(); err == nil {
+        t.Errorf("Expected createGocronTable() to return an error, due to no database to connect to")
+    }
+}
