771cfc16b5b9f717414ca11323c6576c4d0710f5
jsirianni <joe.sirianni@bluemedora.com>
2019-03-31 19:36:43 -0400

    add libgocron.go tests

--- libgocron/libgocron_test.go
+++ libgocron/libgocron_test.go
@@ -35,8 +35,67 @@ func getTestCron() Cron {
 
 func TestValidate(t *testing.T) {
     g := getTestConfig()
-
     if err := g.Validate(); err != nil {
         t.Errorf("Expected Validate() to return nil when using a valid config")
+    }
+
+    // Invalid parameters
+    g = getTestConfig()
+    g.Dbdatabase = ""
+    if g.Validate() == nil {
+        t.Errorf("Expected Validate() to return an error, when using bad Dbdatabase")
+    }
+
+    g = getTestConfig()
+    g.Dbfqdn = ""
+    if g.Validate() == nil {
+        t.Errorf("Expected Validate() to return an error, when using bad Dbfqdn")
+    }
+
+    g = getTestConfig()
+    g.Dbpass = ""
+    if g.Validate() == nil {
+        t.Errorf("Expected Validate() to return an error, when using bad Dbpass")
+    }
+
+    g = getTestConfig()
+    g.Dbport = ""
+    if g.Validate() == nil {
+        t.Errorf("Expected Validate() to return an error, when using bad Dbport")
+    }
+
+    g = getTestConfig()
+    g.Dbuser = ""
+    if g.Validate() == nil {
+        t.Errorf("Expected Validate() to return an error, when using bad Dbuser")
+    }
+
+    g = getTestConfig()
+    g.Interval = -1
+    if g.Validate() == nil {
+        t.Errorf("Expected Validate() to return an error, when using bad Interval")
+    }
+
+    g = getTestConfig()
+    g.SlackChannel = ""
+    if g.Validate() == nil {
+        t.Errorf("Expected Validate() to return an error, when using bad SlackChannel")
+    }
+
+    g = getTestConfig()
+    g.SlackChannel = ""
+    if g.Validate() == nil {
+        t.Errorf("Expected Validate() to return an error, when using bad SlackChannel")
+    }
+
+    g = getTestConfig()
+    g.SlackHookURL = ""
+    if g.Validate() == nil {
+        t.Errorf("Expected Validate() to return an error, when using bad SlackHookURL")
+    }
+
+    g.SlackHookURL = "4"
+    if g.Validate() == nil {
+        t.Errorf("Expected Validate() to return an error, when using bad SlackHookURL")
     }
 }
