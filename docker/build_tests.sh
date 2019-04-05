e992cf6ea3341a5c15cdb68e4ef135712fabf55d
Joseph Sirianni <joe.sirianni@bluemedora.com>
2019-04-04 21:13:19 -0400

    fix path

--- docker/build_tests.sh
+++ docker/build_tests.sh
@@ -59,7 +59,7 @@ sleep 8
 
 # check for missed jobs
 echo "checking for missed jobs via api"
-curl -s localhost:3000/crons/alerted | jq . || exit 1
+curl -s localhost:3000/crons/missed | jq . || exit 1
 
 # test for 201 status code when sending a valid GET
 # test for "back online" alert
